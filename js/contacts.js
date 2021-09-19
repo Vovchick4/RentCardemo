097cb9916924a4f1d3ff13de4d5f9c9635f207be
rk800-afk <gtv200013@gmail.com>
2021-09-20 00:18:28 +0300

    valid callback form2

--- js/contacts.js
+++ js/contacts.js
@@ -1,94 +1,74 @@
+const regEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+const regTel = /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/
 const nameInput = document.getElementById("callback_input_name")
 const telInput = document.getElementById("callback_input_tel")
 const emailInput = document.getElementById("callback_input_email")
 const textareaInput = document.getElementById("callback_input_textarea")
 const form = document.getElementById("callback_form")
 const nameError = document.getElementById("callback_name_error")
-const tellError = document.getElementById("callback_tell_error")
+const telError = document.getElementById("callback_tell_error")
 const emailError = document.getElementById("callback_email_error")
 const textareaError = document.getElementById("callback_textarea_error")
 
 form.addEventListener("submit" , (e) => {
-    ShowError(e);
+    ValidateForm(e);
 })
 
-function ShowError(e)
+function ValidateForm(e)
 {
     if(nameInput.value === "" || nameInput.value == null)
     {
-        GetError(nameError, "Заповніть це поле", e)
+        notValid(nameInput, nameError, "Заповніть це поле", e)
     }
 
-    else if(telInput.value === "" || telInput.value == null)//чи не пусте
+    if(!regEmail.test(emailInput.value))
     {
-        GetError(tellError, "Заповніть це поле", e)
+        notValid(emailInput, emailError, "Невірно введений email", e)
     }
 
-    else if(!Number.isInteger(Number(telInput.value)))//чи є цілим числом
+    if(!regTel.test(telInput.value))
     {
-        GetError(tellError, "Неправильно введено номер", e)
+        notValid(telInput, telError, "Невірно введений телефон", e)
     }
 
-    else if(!(Math.sign(Number(telInput.value)) === 1))//чи є не відємним числом
-    {
-        GetError(tellError, "Неправильно введено номер", e)
+    if(textareaInput.value.length > 500){
+        notValid(textareaInput, textareaError, "Перевищено ліміт в 500 символів", e)
     }
-
-    else if(telInput.value.length > 12 || telInput.value.length<10)//чи правильна довжина
-    {
-        GetError(tellError, "Неправильна довжина номеру", e)
-    }
-
-    else if(emailInput.value.indexOf("@", 0) === -1)
-    {
-        GetError(emailError, "Не валідний емейл", e)
-    }
-
-    else if(!(emailInput.value.indexOf("@", 0) > 0 && emailInput.value.indexOf("@", 0) < (emailInput.value.length-1)))
-    {
-        GetError(emailError, "Не валідний емейл", e)
-    }
-
-    else if(emailInput.value.slice(0, emailInput.value.indexOf("@", 0)+1).indexOf(" ") !=-1 || emailInput.value.slice(emailInput.value.indexOf("@", 0), emailInput.value.length).indexOf(" ") !== -1)
-    {
-        GetError(emailError, "Не валідний емейл", e)
-    }
-
-    else if(textareaInput.value.length > 400)
-    {
-        GetError(textareaError, "Введіть менше 400 символів", e)
-    }
-
-    // console.log(emailInput.value.slice(0, emailInput.value.indexOf("@", 0)+1));
-    // e.preventDefault()
-
-    // || emailInput.value.slice(emailInput.value.indexOf("@", 0), emailInput.value.length).indexOf(" ") !== -1)
 }
 
-function GetError(nameError, errorMes, e){
+function validate(regex, input)
+{
+    return regex.test(input)
+}
+
+function notValid(input, el, mess, e)
+{
     e.preventDefault()
-    nameError.innerText = errorMes
-    nameError.style.visibility = "visible";
-    nameError.style.opacity = "1"
+    input.classList.add("callback_input_error")
+    el.innerText = mess
+    el.style.visibility = "visible";
+    el.style.opacity = "1"
 }
 
+function valid(input, el)
+{
+    input.classList.remove("callback_input_error")
+    el.style.opacity = "0"
+    el.style.visibility = "hidden";
+}
 
 nameInput.addEventListener("focus", () => {
-    nameError.style.opacity = "0"
-    nameError.style.visibility = "hidden";
+    valid(nameInput, nameError)
 })
 
 telInput.addEventListener("focus", () => {
-    tellError.style.opacity = "0";
-    nameError.style.visibility = "hidden";
+    valid(telInput, telError)
 })
 
 emailInput.addEventListener("focus", () => {
-    emailError.style.opacity = "0";
-    nameError.style.visibility = "hidden";
+    valid(emailInput, emailError)
 })
 
 textareaInput.addEventListener("focus", () => {
-    textareaError.style.opacity = "0";
-    nameError.style.visibility = "hidden";
+    valid(textareaInput, textareaError)
 })
